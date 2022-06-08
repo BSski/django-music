9a21f532b0c0b2ec3a8fffc6ccfeb18b272b4b8e
bsski <50927653+BSski@users.noreply.github.com>
2022-06-08 20:58:35 +0200

    added homepage

--- website/music/views.py
+++ website/music/views.py
@@ -2,4 +2,4 @@ from django.http.response import HttpResponse
 
 
 def home(request):
-    return HttpResponse("<br><h2 align=\"center\">Welcome to Django Music Website!</h2>")
+    return HttpResponse('<br><h2 align="center">Welcome to Django Music Website!</h2>')
