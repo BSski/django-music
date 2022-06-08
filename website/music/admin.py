d5f817583935420e369df907f2f3af7a38e9f7ae
bsski <50927653+BSski@users.noreply.github.com>
2022-06-08 17:07:10 +0200

    added models to admin panel

--- website/music/admin.py
+++ website/music/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Song, Author
+
+
+class SongInline(admin.TabularInline):
+    model = Song.authors.through
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    """Song admin."""
+    list_display = ('title', 'created_at')
+    search_fields = ('title', 'authors', 'created_at')
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Author admin."""
+    list_display = ('name', 'surname')
+    search_fields = ('name',)
+    inlines = [
+        SongInline,
+    ]
