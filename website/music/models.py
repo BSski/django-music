993fe15493278c2499a26dc7a8477a4f4bf7bff4
bsski <50927653+BSski@users.noreply.github.com>
2022-06-08 17:01:43 +0200

    added models

--- website/music/models.py
+++ website/music/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=300)
+    surname = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=1000)
+    created_at = models.DateField()
+    authors = models.ManyToManyField(Author, blank=True)
+
+    def __str__(self):
+        return self.title
