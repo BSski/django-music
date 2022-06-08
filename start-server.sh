abc0902c4c3853bf4f97a5f750ed12d2e080cf95
bsski <50927653+BSski@users.noreply.github.com>
2022-06-08 19:48:46 +0200

    start server script tweaks

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-python manage.py collectstatic --no-input
+(cd website; python manage.py collectstatic --no-input)
 
-(cd api; gunicorn api.wsgi:application --bind 0.0.0.0:$PORT --workers 3)
+
+(cd website; gunicorn website.wsgi:application --bind 0.0.0.0:$PORT --workers 3)
