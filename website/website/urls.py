01e631848d674750417278272bcd1fa303be28d7
bsski <50927653+BSski@users.noreply.github.com>
2022-06-08 18:21:28 +0200

    moved admin url to env vars

--- website/website/urls.py
+++ website/website/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path
 
+
+ADMIN_LOGIN_URL = os.environ.get("ADMIN_LOGIN_URL")
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f"{ADMIN_LOGIN_URL}/", admin.site.urls),
 ]
