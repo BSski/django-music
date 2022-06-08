4d997da8ca468479fe1d52bfcc13c9927d3e7b98
bsski <50927653+BSski@users.noreply.github.com>
2022-06-09 01:51:45 +0200

    added model tests

--- website/music/tests/test_models.py
+++ website/music/tests/test_models.py
@@ -3,32 +3,36 @@ from datetime import date
 from music.models import Song, Author
 
 
-class SongModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        pass
-        # cls.test_song = Song(
-        #     id="1",
-        #     title="TestTitle",
-        #     authors=["TestAuthor"],
-        #     created_at=date(2022, 3, 22),
-        # )
-
-    def test_song_str_equals_song_title(self):
-        # self.assertEqual(str(self.test_song), self.test_song.title)
-        self.assertEqual(1, 1)
-
-
 class AuthorModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        pass
-        # cls.test_author = Author(
-        #     id="1",
-        #     name="TestName",
-        #     surname="TestSurname",
-        # )
+        cls.test_author = Author.objects.create(
+            name="TestName",
+            surname="TestSurname",
+        )
 
     def test_author_str_equals_name_and_surname(self):
-        # self.assertEqual(str(self.test_author), f"{self.name} {self.surname}")
-        self.assertEqual(1, 1)
+        self.assertEqual(
+            str(self.test_author), f"{self.test_author.name} {self.test_author.surname}"
+        )
+
+
+class SongModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_song = Song.objects.create(
+            title="TestTitle",
+            created_at=date(2022, 6, 8),
+        )
+
+    def test_song_str_equals_song_title(self):
+        self.assertEqual(str(self.test_song), self.test_song.title)
+
+    def test_song_has_an_author(self):
+        test_author = Author.objects.create(
+            name="TestName",
+            surname="TestSurname",
+        )
+        self.test_song.authors.set([test_author])
+
+        self.assertEqual(self.test_song.authors.count(), 1)
---
+++ website/music/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class BooksListHttpMethodsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.home_url = reverse("home")
+
+    def test_can_see_home_view(self):
+        response = self.client.get(self.home_url, secure=True)
+        self.assertEqual(response.status_code, 200)
+
