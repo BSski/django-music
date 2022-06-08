13548e19530a259dc9b8b7a7446bac6c26a63354
bsski <50927653+BSski@users.noreply.github.com>
2022-06-08 17:06:44 +0200

    added migration and adding superuser through code

--- website/music/apps.py
+++ website/music/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.core import management
 
 
 class MusicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'music'
+
+    def ready(self):
+        management.call_command("makemigrations")
+        management.call_command("migrate")
+        management.call_command("createsuperuser", "--noinput")
